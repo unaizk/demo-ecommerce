cdf5eee5681aa9a3b44050a943e07509dc926fe4
root <root@Unaislaptop>
2024-01-25 15:32:16 +0530

    created protec middleware for user auth protection

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -52,8 +52,15 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const logoutUser = asyncHandler(async(req,res) =>{
 
-    res.status(200).json({message:'User logout'})
+    res.cookie('jwt','', {
+      httpOnly : true,
+      expires : new Date(0)
+    })
+    res.status(200).json({message:'User logged out'})
 })
+
+
+
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import userRouter from './routes/userRoutes.js'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 connectDB()
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
 app.use('/api/users',userRouter)
 
